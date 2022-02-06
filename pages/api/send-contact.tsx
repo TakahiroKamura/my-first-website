d589e5d6a36e2f2ad03dcb5dd80e898dd5d7095e
takahiro.kamura <takahiro.kamura@gmail.com>
2022-02-06 21:45:18 +0900

    型指定を追加

--- pages/api/send-contact.tsx
+++ pages/api/send-contact.tsx
@@ -1,4 +1,10 @@
-const handler = (req: any, res: any) => {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+    user: string
+}
+
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
     res.status(200).json({user: 'Ada Lovelace'});
 }
 
