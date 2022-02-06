7eba77ade1aaa7573238fb4ae6560cb4ddf58dab
takahiro.kamura <takahiro.kamura@gmail.com>
2022-02-06 16:08:15 +0900

    <a>タグを使っていてビルドに失敗する不具合を修正

--- pages/sent-contact.tsx
+++ pages/sent-contact.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import Header from "./header";
 import Footer from "./footer";
+import Link from "next/link";
 
 const SentContact: NextPage = () => {
     return (
@@ -11,7 +12,9 @@ const SentContact: NextPage = () => {
                 <p>いただいたお問い合わせを承りました。</p>
                 <p>返信にはしばしお時間をいただく場合、または内容によっては返信できない場合があることを予めご了承ください。</p>
                 <p>今後ともPMK GAMESをよろしくお願いいたします。</p>
-                <a href="/">ホームへ戻る</a>
+                <div className="link">
+                    <Link href="/">ホームへ戻る</Link>
+                </div>
             </div>
             <Footer/>
         </div>
